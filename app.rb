9288b4f397c6b0a4a6ce336f0b821c5a89a7d9b1
Yurina Shin <yurinas@gmail.com>
2018-08-14 20:33:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
 
   end
 
-  get '/say/:word1%/:word2%/:word3%/:word4%/:word5' do
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
     @w1 = params[:word1]
     @w2 = params[:word2]
     @w3 = params[:word3]
